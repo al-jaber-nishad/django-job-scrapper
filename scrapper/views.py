aaf72b5a8fc37a46e77e6f05699dc321e93798e6
al-jaber-nishad <mailnishad02@gmail.com>
2021-11-11 01:26:58 +0600

    Updated

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+import requests
+from bs4 import BeautifulSoup
 # Create your views here.
+
+t_r = requests.get("https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=")
+t_soup = BeautifulSoup(t_r.content, 'lxml')
+jobs = t_soup.find_all('li', class_="clearfix job-bx wht-shd-bx")
+
+news_link = []
+news_title = []
+skills = []
+result = {}
+for job in jobs:
+  title = job.find('h2').strong.text
+  link = job.find('h2').a["href"]
+  # news_link.append(link)
+  # news_title.append(title)
+  skill = job.find('span', class_="srp-skills").text.replace(" ", "")
+  # skills.append(skill)
+
+  result[title] = [link, skill]
+
+def index(request):
+  return render(request, 'scrapper/index.html', {'result':result})
--- webscrapper/settings.py
+++ webscrapper/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'scrapper',
 ]
 
 MIDDLEWARE = [
@@ -118,13 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_DIR = os.path.join(BASE_DIR, 'staic')
-STATICFILEDIRS = [
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
     STATIC_DIR,
 
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
